ba4467d178785dbe2af4a6398e7acbffe94833d2
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2022-11-29 21:27:55 +0800

    Test that the returned `Board` is correct

--- test/BehaviorSpec.hs
+++ test/BehaviorSpec.hs
@@ -1,4 +1,6 @@
-module BehaviorSpec where
+module BehaviorSpec (
+  spec,
+  ) where
 
 import Data.Either
 
@@ -8,6 +10,7 @@ import Test.QuickCheck
 
 import Behavior
 import Model
+import Model.Internal
 
 spec :: Spec
 spec = do
@@ -17,24 +20,33 @@ testExecuteMove :: Spec
 testExecuteMove = do
   describe "executeMove" $ do
     prop "sets an arbitrary cell on an empty board if in range" $
-       \move size -> (executeMove move (newBoard size)
+       \(AnyMove move) size -> (executeMove move (newBoard size)
                        `shouldSatisfy`
                        case move of
-                         (Move _ (Position row col)) | row < size && col < size ->  isRight
+                         (Move s p@(Position row col)) | row < size && col < size -> f s p
                          _ -> isLeft)
+      where
+        f s p (Right b) = getCell b p == Just (Just s)
+        f _ _ _ = False
 
-instance Arbitrary Move where
+newtype AnyMove = AnyMove Move deriving (Show)
+
+instance Arbitrary AnyMove where
   arbitrary = do
-    s <- arbitrary
-    p <- arbitrary
-    return (Move s p)
+    AnySymbol s <- arbitrary
+    AnyPosition p <- arbitrary
+    return (AnyMove $ Move s p)
 
-instance Arbitrary Symbol where
+newtype AnySymbol = AnySymbol Symbol deriving (Show)
+
+instance Arbitrary AnySymbol where
   arbitrary = do
-    elements [Cross, Knot]
+    elements $ map AnySymbol [Cross, Knot]
 
-instance Arbitrary Position where
+newtype AnyPosition = AnyPosition Position deriving (Show)
+
+instance Arbitrary AnyPosition where
   arbitrary = do
     Positive row <- arbitrary
     Positive col <- arbitrary
-    return $ Position row col
+    return (AnyPosition $ Position row col)
